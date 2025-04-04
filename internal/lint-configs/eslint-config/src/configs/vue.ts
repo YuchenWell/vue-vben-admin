a077906b4d482e7ae5414f00556041d0a4365431
zhouzz <275028888@qq.com>
2025-04-04 20:47:22 +0800

    chore: update vue file order

--- internal/lint-configs/eslint-config/src/configs/vue.ts
+++ internal/lint-configs/eslint-config/src/configs/vue.ts
@@ -61,7 +61,7 @@ export async function vue(): Promise<Linter.Config[]> {
         'vue/block-order': [
           'error',
           {
-            order: ['script', 'template', 'style'],
+            order: ['template', 'script', 'style'],
           },
         ],
         'vue/component-name-in-template-casing': ['error', 'PascalCase'],
